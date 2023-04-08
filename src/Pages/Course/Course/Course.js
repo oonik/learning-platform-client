a0a2df6aa956d3da4cd5cb6c38ed2fc5d9952d6c
oonik <aniksarkar0177@gmail.com>
2023-04-08 23:30:44 +0600

    .evn.local

--- src/Pages/Course/Course/Course.js
+++ src/Pages/Course/Course/Course.js
@@ -2,25 +2,34 @@ import React from 'react';
 import { Link, useLoaderData } from 'react-router-dom';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
+import { FaDownload } from 'react-icons/fa';
+
 
 
 const Course = () => {
     const course = useLoaderData();
-    const { title, image_url, details, category_id } = course;
+    const { title, image_url, details, category_id, author } = course;
+    console.log(course)
     return (
         <div>
             <Card>
-            <Card.Img variant="top" src={image_url} />
-            <Card.Body>
-                <Card.Title>{title}</Card.Title>
-                <Card.Text>
-                    {details}
-                </Card.Text>
-                <Link to={`/category/${category_id}`}>
-                <Button variant="primary">Go Category</Button>
-                </Link>
-            </Card.Body>
-        </Card>
+                <Card.Header className='d-flex justify-content-between'>
+                    <h6>{author.name}</h6>
+                    <a href="sample.pdf" download='sample.pdf'>
+                    <button><FaDownload></FaDownload></button>
+                    </a>
+                </Card.Header>
+                <Card.Img variant="top" src={image_url} />
+                <Card.Body>
+                    <Card.Title>{title}</Card.Title>
+                    <Card.Text>
+                        {details}
+                    </Card.Text>
+                    <Link to={`/category/${category_id}`}>
+                        <Button variant="primary">Go Category</Button>
+                    </Link>
+                </Card.Body>
+            </Card>
         </div>
     );
 
--- src/firebase/firebase/firebase.config.js
+++ src/firebase/firebase/firebase.config.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCUwbXcR0c1NFMNZbZCn0I5yv8HSzl_pXU",
-  authDomain: "learning-platform-a2fa0.firebaseapp.com",
-  projectId: "learning-platform-a2fa0",
-  storageBucket: "learning-platform-a2fa0.appspot.com",
-  messagingSenderId: "72673038241",
-  appId: "1:72673038241:web:0cfbff4b9416aa0a4285e0"
+  apiKey: process.env.REACT_APP_apiKey,
+  authDomain: process.env.REACT_APP_authDomain,
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  appId: process.env.REACT_APP_appId
 };
 
 // Initialize Firebase
